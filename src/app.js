1cfedad45a2dd62a87e0a8c661e74a6b5083e786
Rafael Criado <rafael.marquescriado@gmail.com>
2020-04-22 16:07:39 -0300

    Feat: CRUD Repositories and POST for create a like.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,121 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  try {
+    return response.json(repositories);
+  } catch (error) {
+    return response.status(500).json({
+      message : 'Internal server error!',
+    });
+  }
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  try {
+    const { title, url, techs } = request.body;
+    // todos os campos são obrigatórios
+    if(!title || title === '' || !url || url === '' || !techs || techs.length <=0){
+      return response.status(400).json({
+        message : 'The title, url and techs fields are required',
+      });
+    }
+
+    // Campo tech deve ser array
+    if(!Array.isArray(techs)){
+      return response.status(400).json({
+        message : 'The field techs must be an array',
+      });
+    }
+
+    const repository = { id: uuid(), title, url, techs, likes: 0 }; 
+    repositories.push(repository);
+
+    return response.json(repository);
+  } catch (error) {
+    return response.status(500).json({
+      message : 'Internal server error!',
+    });
+  }
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  try {
+    
+    const { id } = request.params;
+
+    const repository = repositories.find(r => r.id === id);
+
+    if(!repository){
+      return response.status(400).json({
+        message : 'The repository does not exist!',
+      });
+    }
+
+    const { title, url , techs } = request.body;
+
+    if(techs){
+      // Caso passe o campo tech o mesmo deve 
+      if(!Array.isArray(techs) || techs.length <= 0){
+        return response.status(400).json({
+          message : 'The field techs must be an array',
+        });
+      }
+      repository.techs = techs;
+    }
+
+    if(title && title !== '') repository.title = title;
+    if(url && url !== '') repository.url = url;
+
+    return response.json(repository);
+
+  } catch (error) {
+    return response.status(500).json({
+      message : 'Internal server error!',
+    });
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  try {
+    const { id } = request.params;
+
+    const indexRepo = repositories.findIndex(r => r.id === id);
+
+    if(indexRepo < 0){
+      return response.status(400).json({
+        message : 'The repository does not exist!',
+      });
+    }
+    repositories.splice(indexRepo, 1);
+    return response.status(204).json();
+    
+  } catch (error) {
+    return response.status(500).json({
+      message : 'Internal server error!',
+    });
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  try {
+    const { id } = request.params;
+
+    const repository = repositories.find(r => r.id === id);
+
+    if(!repository){
+      return response.status(400).json({
+        message : 'The repository does not exist!',
+      });
+    }
+
+    repository.likes = repository.likes + 1;
+    return response.json(repository);
+
+  } catch (error) {
+    return response.status(500).json({
+      message : 'Internal server error!',
+    });
+  }
 });
 
 module.exports = app;
